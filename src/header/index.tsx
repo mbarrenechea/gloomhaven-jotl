08a19fa5fcce6080b9f0804181c50a755b9d40ee
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-04-18 01:28:31 +0200

    Dark mode

--- src/header/index.tsx
+++ src/header/index.tsx
@@ -16,15 +16,15 @@ export const Header = () => {
   };
 
   return (
-    <header className="bg-neutral-900 py-4 text-white">
+    <header className="bg-card py-4 text-foreground">
       <div className="container">
         <div className="flex items-center justify-between">
           <h1 className="font-display text-4xl">Gloomhaven: combat</h1>
 
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-4">
             <Label
               htmlFor="players"
-              className="font-display block text-sm leading-0 tracking-widest uppercase"
+              className="font-sans block text-md leading-0 tracking-widest uppercase"
             >
               Players:
             </Label>
@@ -42,9 +42,8 @@ export const Header = () => {
                     className={cn({
                       "font-display h-10 w-10 cursor-pointer items-center justify-center rounded-full border border-transparent text-base transition-colors duration-300 ease-in-out":
                         true,
-                      "hover:text-foreground hover:bg-neutral-300 focus:ring-2 focus:ring-neutral-500 focus:ring-offset-2 focus:ring-offset-neutral-500":
-                        players !== player,
-                      "text-foreground border-neutral-300 bg-amber-500": players === player,
+                      "hover:text-foreground hover:bg-secondary": players !== player,
+                      "text-foreground-muted border-border bg-primary": players === player,
                     })}
                     htmlFor={player}
                   >
