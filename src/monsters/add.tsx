06721479b438b3465445c4ac0394fbffdc2f3580
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-04-18 01:56:05 +0200

    Monster add button

--- src/monsters/add.tsx
+++ src/monsters/add.tsx
@@ -1,17 +1,43 @@
 import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { LucidePlus } from "lucide-react";
 
 export const AddMonster = () => {
   return (
     <div className="fixed w-full bottom-10 pointer-events-none">
       <div className="container flex justify-end">
-        <Button
-          size="icon"
-          variant="default"
-          className="rounded-full w-16 h-16 pointer-events-auto"
-        >
-          <LucidePlus className="size-8" />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              size="icon"
+              variant="default"
+              className="rounded-full w-16 h-16 pointer-events-auto"
+            >
+              <LucidePlus className="size-8" />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent className="p-4">
+            <SheetTitle className="text-4xl font-display">Add new monsters</SheetTitle>
+            <SheetDescription className="text-xl sr-only">
+              Use this sheet to add a{" "}
+              <strong className="text-foreground">new monster to the game.</strong> You can select
+              the monster type and level, and the sheet will automatically calculate the monster's
+              stats based on the selected level. You can also add any special abilities or
+              characteristics that the monster may have. Once you have filled out all the necessary
+              information, click the "Add Monster" button to add the monster to the game. The new
+              monster will be added to the list of active monsters, and you can start using it in
+              your game right away. If you need to make any changes to the monster later, you can
+              always edit its stats or abilities from the active monsters list.
+            </SheetDescription>
+          </SheetContent>
+        </Sheet>
       </div>
     </div>
   );
