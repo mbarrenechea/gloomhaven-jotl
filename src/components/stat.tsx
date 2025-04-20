3be92fb8bbffe686570d5b03e6a4229925438b57
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-04-20 02:34:36 +0200

    Helth change

--- src/components/stat.tsx
+++ src/components/stat.tsx
@@ -5,15 +5,15 @@ import { PropsWithChildren } from "react";
 const statVariants = cva("font-display px-4 py-1 text-2xl flex items-center justify-center gap-1", {
   variants: {
     variant: {
-      default: "bg-accent",
+      default: "text-foreground",
       health: "text-red-400 fill-red-400",
       movement: "text-blue-400 fill-blue-800",
       attack: "text-foreground fill-red-900",
     },
     size: {
-      default: "text-xl",
+      default: "text-xl py-2 px-4",
       sm: "text-3xl py-0 px-2",
-      lg: "text-5xl",
+      lg: "text-5xl py-1 px-4",
     },
   },
   defaultVariants: {
--- src/monsters/list/item.tsx
+++ src/monsters/list/item.tsx
@@ -1,8 +1,35 @@
 import { Stat } from "@/components/stat";
 import { cn } from "@/lib/utils";
 import { Monster } from "@/monsters/types";
+import { useLocalStorage } from "usehooks-ts";
 
-export const MonsterItem = ({ type, index, image, health, movement, attack }: Monster) => {
+export const MonsterItem = ({ id, type, index, image, health, movement, attack }: Monster) => {
+  const [, setMonsters] = useLocalStorage<Monster[]>("monsters", []);
+  const handleHealthChange = (v: number) => {
+    // Handle health increment logic here
+    console.log(id, health);
+    setMonsters((prev) => {
+      const monsterIndex = prev.findIndex((monster) => monster.id === id);
+      if (monsterIndex === -1) return prev;
+
+      const newMonsters = [...prev];
+      const h = newMonsters[monsterIndex].health + v;
+      if (h < 0) {
+        newMonsters[monsterIndex].health = 0;
+
+        return newMonsters;
+      }
+
+      if (h >= 0) {
+        newMonsters[monsterIndex].health = h;
+
+        return newMonsters;
+      }
+
+      return newMonsters;
+    });
+  };
+
   return (
     <div
       className={cn({
@@ -14,7 +41,7 @@ export const MonsterItem = ({ type, index, image, health, movement, attack }: Mo
       <div className="w-full h-full aspect-square rounded-full overflow-hidden">
         <img src={image} alt={type} className="w-full object-cover" />
       </div>
-      <div className="absolute w-full h-full top-0 left-0 grid grid-cols-2 gap-2">
+      <div className="absolute w-full h-full top-0 left-0 grid grid-cols-2 gap-2 pointer-events-none">
         <div className="flex flex-col items-start justify-start w-full h-full text-foreground bg-gradient-to-r from-black/50 to-transparent pt-2">
           <Stat variant="health" size="lg">
             {health}
@@ -26,9 +53,24 @@ export const MonsterItem = ({ type, index, image, health, movement, attack }: Mo
             {attack}
           </Stat>
         </div>
-        <div className="flex flex-col items-end justify-between w-full h-full text-foreground">
-          <Stat variant="default">{index}</Stat>
+        <div className="flex flex-col items-end justify-between w-full h-full text-foreground pt-2">
+          <Stat variant="default" size="default">
+            {index}
+          </Stat>
         </div>
+      </div>
+
+      <div className="absolute z-10 w-full h-full top-0 left-0 flex flex-col items-center justify-center">
+        <button
+          type="button"
+          className="grow w-full bg-transparent from-background/50 to-transparent active:bg-gradient-to-b"
+          onClick={() => handleHealthChange(1)}
+        />
+        <button
+          type="button"
+          className="grow w-full bg-transparent from-background/50 to-transparent active:bg-gradient-to-t"
+          onClick={() => handleHealthChange(-1)}
+        />
       </div>
     </div>
   );
